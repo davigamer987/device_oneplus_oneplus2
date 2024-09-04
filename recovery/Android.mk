786d34b617e87d9ce5ca0b116284eb1bcb0c3f4e
Christian Hoffmann <chrmhoffmann@gmail.com>
2024-09-04 16:13:47 -0500

    oneplus2: Align with new recovery structure
    Change-Id: Ic3ecf63a2888aac08aeaf40677ae7bfa74912030

--- recovery/Android.mk
+++ recovery/Android.mk
@@ -7,9 +7,10 @@ LOCAL_CFLAGS += -DUSES_BOOTDEVICE_PATH
 endif
 
 LOCAL_C_INCLUDES := \
-    bootable/recovery \
     bootable/deprecated-ota/edify/include \
-    bootable/recovery/otautil/include 
+    bootable/recovery \
+    bootable/recovery/otautil/include
+
 LOCAL_SRC_FILES := recovery_updater.cpp
 LOCAL_MODULE := librecovery_updater_oneplus2
 include $(BUILD_STATIC_LIBRARY)
