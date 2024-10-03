39c6d5c2d6550868060b77f825e343f42e842708
Derek <nielson925@gmail.com>
2024-10-03 22:30:42 +0300

    oneplus2: Use lineage radio 1.4 wrapper
    Change-Id: Id51367011e82d7d474eb9fa33d49079f1277fba0

--- device.mk
+++ device.mk
@@ -285,8 +285,8 @@ PRODUCT_COPY_FILES += \
 
 # Net
 PRODUCT_PACKAGES += \
+    android.hardware.radio@1.4-service.msm8994 \
     android.hardware.radio.config@1.0-service
-
 
 # OMX
 PRODUCT_PACKAGES += \
