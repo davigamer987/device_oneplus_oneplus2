9c079c6878810851ef7a8b313e02056931757cc5
Davi <davimarchesiprimo@gmail.com>
2026-06-30 21:09:34 -0300

    oneplus2: switch PRODUCT_PROP path to absolute path
    Android 16 seems to not like LOCAL_PATH anymore

--- device.mk
+++ device.mk
@@ -291,7 +291,7 @@ PRODUCT_PACKAGES += \
     android.hardware.radio@1.4-service.msm8994 \
     android.hardware.radio.config@1.1-service.msm8994
 
-TARGET_PRODUCT_PROP += $(LOCAL_PATH)/product.prop
+TARGET_PRODUCT_PROP += device/oneplus/oneplus2/product.prop
 
 # OMX
 PRODUCT_PACKAGES += \
